eb101a292048a56fe74d562a00f1e862095343b7
Victor Nordstrand <victor.nordstrand@apendo.dk>
2020-08-29 19:02:28 +0200

    additional get, post and put methods added

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using product_api_netcore.Models;
+using System;
 using System.Linq;
 
 namespace product_api_netcore.Controllers
@@ -43,6 +44,83 @@ namespace product_api_netcore.Controllers
               .OrderBy(p => p.ProductNumber)
               .Skip(request.Offset)
               .Take(request.Limit));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Product> PostProduct([FromBody] Product product)
+        {
+            try
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                return new CreatedResult($"/products/{product.ProductNumber.ToLower()}", product);
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning(e, "Unable to POST product.");
+
+                return ValidationProblem(e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("product-number/{productNumber}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Product> GetProductByProductNumber([FromRoute] string productNumber)
+        {
+            var productDb = _context.Products
+              .FirstOrDefault(p => p.ProductNumber.Equals(productNumber,
+                        StringComparison.InvariantCultureIgnoreCase));
+
+            if (productDb == null) return NotFound();
+
+            return Ok(productDb);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Product> GetProductById([FromRoute] int id)
+        {
+            var productDb = _context.Products
+              .FirstOrDefault(p => p.Id == id);
+
+            if (productDb == null) return NotFound();
+
+            return Ok(productDb);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Product> PutProduct([FromBody] Product product)
+        {
+            try
+            {
+                var productDb = _context.Products
+                  .FirstOrDefault(p => p.Id == product.Id);
+
+                if (productDb == null) return NotFound();
+
+                productDb.Name = product.Name;
+                productDb.Price = product.Price;
+                productDb.Department = product.Department;
+                _context.SaveChanges();
+
+                return Ok(product);
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning(e, "Unable to PUT product.");
+
+                return ValidationProblem(e.Message);
+            }
         }
 
         public class ProductRequest
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace product_api_netcore.Models
 {
     public class Product
     {
+
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
         [Required]
         [Display(Name = "productNumber")]
         public string ProductNumber { get; set; }
